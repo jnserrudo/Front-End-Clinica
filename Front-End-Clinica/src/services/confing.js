da6fe17478beb793adcbb5b9f1c2d7227f4ac087
José Nahuel Serrudo <jnserrudo@ucasal.local>
2024-07-23 08:40:13 -0300

    tabla loading config

--- Front-End-Clinica/src/services/confing.js
+++ Front-End-Clinica/src/services/confing.js
@@ -1,5 +1,5 @@
 //export let entorno='http://localhost:3001'
 
-export let entorno='http://localhost:1234/api'
+//export let entorno='http://localhost:1234/api'
 
-//export let entorno='https://back-end-clinica.onrender.com/api'
+export let entorno='https://back-end-clinica.onrender.com/api'
